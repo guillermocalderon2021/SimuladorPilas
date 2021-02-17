b612c9c081407cdcff4a5fbf3135b605ff2aef07
Guillermo Calderon <guillermo.calderon@udb.edu.sv>
2021-02-17 09:49:56 -0600

    inicial
    Commit inicial

--- SimuladorPilas/Form1.Designer.cs
+++ SimuladorPilas/Form1.Designer.cs
@@ -29,12 +29,99 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudValor = new System.Windows.Forms.NumericUpDown();
+            this.btnPush = new System.Windows.Forms.Button();
+            this.btnPop = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudValor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor:";
+            // 
+            // nudValor
+            // 
+            this.nudValor.Location = new System.Drawing.Point(70, 27);
+            this.nudValor.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.nudValor.Name = "nudValor";
+            this.nudValor.Size = new System.Drawing.Size(80, 20);
+            this.nudValor.TabIndex = 1;
+            // 
+            // btnPush
+            // 
+            this.btnPush.Location = new System.Drawing.Point(173, 24);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(75, 23);
+            this.btnPush.TabIndex = 2;
+            this.btnPush.Text = "Push";
+            this.btnPush.UseVisualStyleBackColor = true;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // btnPop
+            // 
+            this.btnPop.Location = new System.Drawing.Point(268, 24);
+            this.btnPop.Name = "btnPop";
+            this.btnPop.Size = new System.Drawing.Size(75, 23);
+            this.btnPop.TabIndex = 3;
+            this.btnPop.Text = "Pop";
+            this.btnPop.UseVisualStyleBackColor = true;
+            this.btnPop.Click += new System.EventHandler(this.btnPop_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(33, 68);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(682, 200);
+            this.panel1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(760, 298);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnPop);
+            this.Controls.Add(this.btnPush);
+            this.Controls.Add(this.nudValor);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Simulador de pilas";
+            ((System.ComponentModel.ISupportInitialize)(this.nudValor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudValor;
+        private System.Windows.Forms.Button btnPush;
+        private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- SimuladorPilas/Form1.cs
+++ SimuladorPilas/Form1.cs
@@ -12,9 +12,80 @@ namespace SimuladorPilas
 {
     public partial class Form1 : Form
     {
+        Stack<int> pila = new Stack<int>();
+        Stack<Label> pilaLabel = new Stack<Label>();
+        int x = 0;
+        int y = 75;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            int valor = Convert.ToInt32(nudValor.Value);
+            pila.Push(valor);
+            Label miLabel = new Label();
+            miLabel.Text = valor.ToString();
+            miLabel.Width = 50;
+            miLabel.Height = 50;
+            miLabel.BackColor = Color.White;
+            miLabel.TextAlign =  ContentAlignment.MiddleCenter;
+            miLabel.BorderStyle = BorderStyle.FixedSingle;
+            miLabel.Location = new Point(0,0);
+            //x += 50;
+            panel1.Controls.Add(miLabel);
+            pilaLabel.Push(miLabel);
+            timer1.Start();
+        }
+
+        private void btnPop_Click(object sender, EventArgs e)
+        {
+            pila.Pop();
+            timer2.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Label miLabel = pilaLabel.Peek();//Recuperando el ultimo label
+            miLabel.BackColor = Color.Blue;
+            if (miLabel.Location.X < x)
+            {
+                miLabel.Location = new Point(miLabel.Location.X + 5, 0);
+                return;
+            }
+            if (miLabel.Location.Y < y)
+            {
+                miLabel.Location = new Point(miLabel.Location.X, miLabel.Location.Y+5);
+                return;
+            }
+
+            x += 50;
+            miLabel.BackColor = Color.White;
+            timer1.Stop();
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Label miLabel = pilaLabel.Peek();
+            miLabel.BackColor = Color.Red;
+            if (miLabel.Location.Y > 0)
+            {
+                miLabel.Location = new Point(miLabel.Location.X, miLabel.Location.Y - 5);
+                return;
+            }
+            if (miLabel.Location.X > 0)
+            {
+                miLabel.Location = new Point(miLabel.Location.X - 5, 0);
+                return;
+            }
+            pilaLabel.Pop();
+            panel1.Controls.Remove(miLabel);
+            x -= 50;
+            timer2.Stop();
         }
     }
 }
